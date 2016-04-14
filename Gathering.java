4eb02cdb7738738075f66d5f81510f3afe9c12b3
thatonekiddevin <ojustmute@gmail.com>
2016-04-13 20:36:28 -0400

    Update Gathering

--- Gathering.java
+++ Gathering.java
@@ -1,3 +1,4 @@
+import java.io.File;
 
 public class Gathering {
 	
@@ -5,17 +6,44 @@ public class Gathering {
 	private Liquid liquidEvent = new Liquid();
 	private Steps stepEvent = new Steps();
 	private EventIO Serializable = new EventIO();
+	private Reporting report;
 	
 	public Gathering() {
-	}
-	
-	public void addSleep() {
+		
+		File stepsFile = new File("Steps.txt");
+		if (stepsFile.exists()) {
+			stepEvent = Serializable.readSteps();
+		}
+		File sleepFile = new File("Sleep.txt");
+		if (sleepFile.exists()) {
+			sleepEvent = Serializable.readSleep();
+		}
+		File liquidFile = new File("Liquid.txt");
+		if (liquidFile.exists()) {
+			liquidEvent = Serializable.readLiquid();
+		}
+		
+		report = new Reporting(liquidEvent, stepEvent, sleepEvent);
 		
 	}
 	
 	public void addLiquid(int addAmount) {
 		liquidEvent.Set(addAmount, liquidEvent.TimeStamp());
 		Serializable.writeLiquid(liquidEvent);
+	}
+	
+	public void addSteps(int addAmount) {
+		stepEvent.Set(addAmount, stepEvent.TimeStamp());
+		Serializable.writeSteps(stepEvent);
+	}
+	
+	public void addSleep(int addAmount) {
+		sleepEvent.Set(addAmount, sleepEvent.TimeStamp());
+		Serializable.writeSleep(sleepEvent);
+	}
+	
+	public String testLiquidReport(String date) {
+		return report.reportLiquidDay(date);
 	}
 	
 	
