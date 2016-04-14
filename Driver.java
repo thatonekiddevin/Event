0d51b4274e1ab935b332e986e4d5fc81b955c9d0
thatonekiddevin <ojustmute@gmail.com>
2016-04-13 20:34:32 -0400

    Update Driver

--- Driver.java
+++ Driver.java
@@ -6,7 +6,10 @@ public class Driver {
 		Gathering gather = new Gathering();
 		
 		gather.addLiquid(15);
+		gather.addSteps(10000);
+		gather.addSleep(5);
 		
-		
+		String test = gather.testLiquidReport("2016/04/13");
+		System.out.println(test);
 	}
 }
