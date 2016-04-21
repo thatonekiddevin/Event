6d07860ead0354524aa57831da8608e62e9693c2
thatonekiddevin <ojustmute@gmail.com>
2016-04-21 17:47:20 -0400

    Update

---
+++ Reporting.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class Reporting {
+	
+	private int[] Calendar = {31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30};
+	
+	public Reporting() {
+		
+	}
+	
+	public String reportDay(String date, ArrayList<Event> eventHolder, String eventType) {
+		String reportString = "";
+		
+		for (int arrayIndex = 0; arrayIndex < eventHolder.size(); arrayIndex++) {
+			if (eventHolder.get(arrayIndex).getClass().getSimpleName().equals(eventType) && eventHolder.get(arrayIndex).getTime().substring(0, 10).equals(date)) {
+				reportString += "Time: " + eventHolder.get(arrayIndex).getTime() + " Amount: " + eventHolder.get(arrayIndex).getAmount() + " | ";
+			}
+		}
+		
+		return reportString;
+	}
+	
+	public String reportMonth(String month, ArrayList<Event> eventHolder, String eventType) {
+		String reportString = "";
+		
+		for (int arrayIndex = 0; arrayIndex < eventHolder.size(); arrayIndex++) {
+			if (eventHolder.get(arrayIndex).getClass().getSimpleName().equals(eventType) && eventHolder.get(arrayIndex).getTime().substring(0,7).equals(month)) {
+				reportString += "Time: " + eventHolder.get(arrayIndex).getTime() + " Amount: " + eventHolder.get(arrayIndex).getAmount() + " | ";
+			}
+		}
+		
+		return reportString;
+	}
+	
+	public String reportYear(String year, ArrayList<Event> eventHolder, String eventType) {
+		String reportString = "";
+		
+		for (int arrayIndex = 0; arrayIndex < eventHolder.size(); arrayIndex++) {
+			if (eventHolder.get(arrayIndex).getClass().getSimpleName().equals(eventType) && eventHolder.get(arrayIndex).getTime().substring(0, 4).equals(year)) {
+				reportString += "Time: " + eventHolder.get(arrayIndex).getTime() + " Amount: " + eventHolder.get(arrayIndex).getAmount() + " | ";
+			}
+		}
+		
+		return reportString;
+	}	
+}
